31e7e456af9908745c16b2f55510fe995b768f74
Priyanshu-choudhary <broyadi23@gmail.com>
2023-06-08 11:18:35 +0530

    almost complete c

---
+++ strings.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(){
+char txt[]="i want to use\"quets\" inside a strings";
+printf("%s",txt);
+}
---
+++ strings/stringToNumber.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+
+char str1[]="only string";
+char str2[]="string with 1233";
+char str3[]="123 some number 34354";
+
+int a=atoi(str1);
+int b=atoi(str2);
+int c=atoi(str3);		
+
+printf("first string:%d\n",a);
+printf("secound string:%d\n",b);
+printf("third string:%d\n",c);
+
+
+
+	}
